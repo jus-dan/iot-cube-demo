9ed63793fd764d99301d990ffdb3862a82d57c68
Danilo Just <92367836+jus-dan@users.noreply.github.com>
2023-01-11 19:26:17 +0100

    clavis Cloud integrated

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
 function Registriere_IoT_Cube (CubeNummer: number) {
     if (CubeNummer == 1) {
         IoTCube.OTAA_Setup(
+        "0000000000000006",
+        "AC1F09FFFE083852",
+        "8B99F3EDC572772101EABF0C878B2B92",
+        eBands.EU868,
+        "A"
+        )
+    }
+    if (CubeNummer == 2) {
+        IoTCube.OTAA_Setup(
         "0000000000000001",
         "70B3D57ED005949E",
         "C34C16032D2016CC2310BE2380E9F6B5",
@@ -18,14 +27,9 @@ function Registriere_IoT_Cube (CubeNummer: number) {
 }
 Registriere_IoT_Cube(1)
 loops.everyInterval(10000, function () {
-    if (IoTCube.getStatus(eSTATUS_MASK.JOINED)) {
-        basic.showIcon(IconNames.SmallSquare)
-        IoTCube.addTemperature(input.temperature(), Channels.One)
-        IoTCube.addAnalogInput(grove.measureInCentimetersV2(DigitalPin.P1), Channels.Two)
-        IoTCube.addIlluminance(pins.analogReadPin(AnalogPin.P2), Channels.Three)
-        IoTCube.SendBuffer(IoTCube.getCayenne(), Channels.One)
-        basic.showIcon(IconNames.SmallDiamond)
-    } else {
-        basic.showIcon(IconNames.Skull)
-    }
+    basic.showIcon(IconNames.SmallSquare)
+    IoTCube.addTemperature(input.temperature(), Channels.One)
+    IoTCube.addAnalogInput(input.soundLevel(), Channels.One)
+    IoTCube.SendBuffer(IoTCube.getCayenne(), Channels.One)
+    basic.showIcon(IconNames.SmallDiamond)
 })
